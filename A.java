3a0ee9aa388b0472fbc0d19031eb8233a3b1c2dd
Mayur Srivastava <mayur.opensource@gmail.com>
2021-09-13 16:50:29 -0400

    Add A+B

---
+++ A.java
@@ -0,0 +1,7 @@
+
+public class A {
+
+    /** CTOR */
+    public A() {}
+
+}
---
+++ B.java
@@ -0,0 +1,12 @@
+
+public class B {
+
+    /** Factory */
+    static B create() {
+        return new B();	
+    }
+    
+    /** CTOR */
+    public B() {}
+
+}
